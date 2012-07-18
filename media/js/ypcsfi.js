feb853b304a622e2ebbd22807564d09d02b4a469
Ville Korhonen <ville@xd.fi>
2012-07-19 01:50:51 +0300

    Use FullCalendar on #event-calendar elements

--- media/js/ypcsfi.js
+++ media/js/ypcsfi.js
@@ -2,7 +2,11 @@
  * @author Ville Korhonen <ville@xd.fi>
  * @version 1.0
  */
+var GOOGLE_CALENDAR_URL = "http://www.google.com/calendar/feeds/xd.fi_1b0msr6fmvatsgsdnn0tcoel4c%40group.calendar.google.com/public/basic";
 
 $(document).ready(function() {
-	// TODO
+    // Event Calendar
+    $('#event-calendar').fullCalendar({
+        events: GOOGLE_CALENDAR_URL
+    });
 });
