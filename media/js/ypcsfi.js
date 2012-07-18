3722c7e387569fa3bce979db575525046b2cef62
Ville Korhonen <ville@xd.fi>
2012-07-19 02:07:08 +0300

    Localized event calendar

--- media/js/ypcsfi.js
+++ media/js/ypcsfi.js
@@ -7,6 +7,26 @@ var GOOGLE_CALENDAR_URL = "http://www.google.com/calendar/feeds/xd.fi_1b0msr6fmv
 $(document).ready(function() {
     // Event Calendar
     $('#event-calendar').fullCalendar({
-        events: GOOGLE_CALENDAR_URL
+        events: GOOGLE_CALENDAR_URL,
+        firstDay: 1, // 0 = Sunday, 1 = Monday
+        allDayText: 'Koko päivä',
+        axisFormat: 'HH:mm',
+        firstHour: 8, // show events after 08:00 as default
+        timeFormat: 'HH:mm',
+        columnFormat: {
+            month: 'ddd',
+            week: 'ddd d.M.',
+            day: 'dddd d.M.'
+        },
+        buttonText: {
+            today: 'tänään',
+            month: 'kuukausi',
+            week: 'viikko',
+            day: 'päivä'
+        },
+        monthNames: ['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu'],
+        monthNamesShort: ['Tam', 'Hel', 'Maa', 'Huh', 'Tou', 'Kes', 'Hei', 'Elo', 'Syy', 'Lok', 'Mar', 'Jou'],
+        dayNames: ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'],
+        dayNamesShort: ['Sun', 'Maa', 'Tii', 'Kes', 'Tor', 'Per', 'Lau']
     });
 });
