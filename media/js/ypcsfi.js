5ba70e53874a4803e8142a8e8edd36d5f10a4bbd
Ville Korhonen <ville@xd.fi>
2012-07-19 10:51:11 +0300

    Removed basicWeek -view from calendar

--- media/js/ypcsfi.js
+++ media/js/ypcsfi.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
         header: {
             left: 'title',
             center: '',
-            right: 'today month,basicWeek,agendaWeek prev,next'
+            right: 'today month,agendaWeek prev,next'
         }
     });
 });
