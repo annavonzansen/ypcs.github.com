e2abf353bde94a93a83a79dddd5bea96e2e22d1e
Ville Korhonen <ville@xd.fi>
2012-07-19 10:48:49 +0300

    Added buttons to display different calendar modes

--- media/js/ypcsfi.js
+++ media/js/ypcsfi.js
@@ -27,6 +27,11 @@ $(document).ready(function() {
         monthNames: ['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu'],
         monthNamesShort: ['Tam', 'Hel', 'Maa', 'Huh', 'Tou', 'Kes', 'Hei', 'Elo', 'Syy', 'Lok', 'Mar', 'Jou'],
         dayNames: ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'],
-        dayNamesShort: ['Sun', 'Maa', 'Tii', 'Kes', 'Tor', 'Per', 'Lau']
+        dayNamesShort: ['Sun', 'Maa', 'Tii', 'Kes', 'Tor', 'Per', 'Lau'],
+        header: {
+            left: 'title',
+            center: '',
+            right: 'today month,basicWeek,agendaWeek prev,next'
+        }
     });
 });
